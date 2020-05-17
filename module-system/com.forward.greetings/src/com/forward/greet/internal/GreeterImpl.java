7964f18a955b2972d38e0858d5a76a363a7a5c88
Kiselev <j8kiselev@gmail.com>
2020-05-17 12:37:48 +0300

    Create Modular Jar file

--- module-system/com.forward.greetings/src/com/forward/greet/internal/GreeterImpl.java
+++ module-system/com.forward.greetings/src/com/forward/greet/internal/GreeterImpl.java
@@ -7,6 +7,12 @@ import com.forward.greet.Greeter;
  * javac com.forward.greetings/src/module-info.java \
  * > com.forward.greetings/src/com/forward/greet/Greeter.java \
  * > com.forward.greetings/src/com/forward/greet/internal/GreeterImpl.java
+ *
+ * CREATE MODULAR Jar file:
+ * Compile to one directory with module-info file:
+ * javac -d out/com.forward.greetings $(find com.forward.greetings -name *.java)
+ * jar -cvf com.forward.greetings.jar -C out/com.forward.greetings .
+ *    create jar file with name              from              locate to
  */
 
 public class GreeterImpl implements Greeter {
--- module-system/com.forward.hello/src/com/forward/hello/HelloWorld.java
+++ module-system/com.forward.hello/src/com/forward/hello/HelloWorld.java
@@ -7,6 +7,13 @@ import com.forward.greet.Greeter;
  * > hello/src/com/forward/hello/HelloWorld.java
  *
  * java -p com.forward.greetings:com.forward.hello -m com.forward.hello/com.forward.hello.HelloWorld
+ *
+ * CREATE MODULAR Jar file:
+ *  javac -p com.forward.greetings.jar \
+ * > -d out/com.forward.hello $(find com.forward.hello -name *.java)
+ *
+ * jar -c -v -f com.forward.hello.jar -e com.forward.hello.HelloWorld -C out/com.forward.hello .
+ * java -p com.forward.greetings.jar:com.forward.hello.jar -m com.forward.hello
  */
 public class HelloWorld {
     public static void main(String[] args) {
