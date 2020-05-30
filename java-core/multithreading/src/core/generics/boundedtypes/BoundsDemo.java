9e20b4a2b84f3b420277061623f6595a2fa0157e
Kiselev <j8kiselev@gmail.com>
2020-05-30 20:10:14 +0300

    Java Core. Generics. Using Wildcard Arguments

--- java-core/multithreading/src/core/generics/boundedtypes/BoundsDemo.java
+++ java-core/multithreading/src/core/generics/boundedtypes/BoundsDemo.java
@@ -1,6 +1,7 @@
 package core.generics.boundedtypes;
 
 public class BoundsDemo {
+
     public static void main(String[] args) {
         Integer[] iNums = {1, 2, 3, 4, 5};
         Stats<Integer> iob = new Stats<>(iNums);
@@ -9,6 +10,24 @@ public class BoundsDemo {
         Double[] dNums = {1.1, 2.2, 3.3, 4.4, 5.5};
         Stats<Double> dob = new Stats<>(dNums);
         System.out.println("dob average is: " + dob.average());
+
+        Float[] fNums = {1.0F, 2.0F, 3.0F, 4.0F, 5.0F};
+        Stats<Float> fob = new Stats<>(fNums);
+        System.out.println("fob average is: " + fob.average());
+
+        System.out.print("Averages of iob and dob ");
+        if (iob.sameAvg(dob)) {
+            System.out.println("are the same");
+        } else {
+            System.out.println("differ");
+        }
+
+        System.out.print("Averages of iob and fob ");
+        if (iob.sameAvg(fob)) {
+            System.out.println("are the same");
+        } else {
+            System.out.println("differ");
+        }
 
         // won't compile because String is not a subclass of Number
         /*String[] sNums = {"1", "2", "3", "4", "5"};
@@ -32,5 +51,12 @@ class Stats<T extends Number> {
         }
 
         return sum / nums.length;
+    }
+
+    boolean sameAvg(Stats<?> obj) {
+        if (average() == obj.average()) {
+            return true;
+        }
+        return false;
     }
 }
