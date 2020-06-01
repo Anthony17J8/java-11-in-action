78da98a600b8e1641d7f0486fb3588c69f93fadb
Kiselev <j8kiselev@gmail.com>
2020-06-01 15:39:31 +0300

    Java Core. Lambdas. Method reference to instance methods

---
+++ java-core/core/src/core/lambda/instmethref/InstanceMethodRef.java
@@ -0,0 +1,28 @@
+package core.lambda.instmethref;
+
+public class InstanceMethodRef {
+
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+
+    public static void main(String[] args) {
+        String str = "Instance Method Reference";
+        MyStringOps mso = new MyStringOps();
+        System.out.println(stringOp(mso::strReverse, str));
+    }
+}
+
+interface StringFunc {
+    String func(String n);
+}
+
+class MyStringOps {
+    String strReverse(String str) {
+        String result = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+}
